87a1f0e2524ae0494c0e6b490e98d68f568b7a10
vasu dev <vasudv0912@gmail.com>
2016-10-11 10:57:38 +0530

    Deeviab android project

---
+++ app/src/main/java/com/lytyfy/deviab/MainActivity.java
@@ -0,0 +1,87 @@
+package com.lytyfy.deviab;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+
+    private static final String REGISTER_URL = "https://dev-api.lytyfy.org/api/lender/token/new";
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+
+    private Button buttonRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextPassword = (EditText) findViewById(R.id.loginPassword);
+        editTextEmail= (EditText) findViewById(R.id.loginEmail);
+
+        buttonRegister = (Button) findViewById(R.id.loginButton);
+
+        buttonRegister.setOnClickListener(this);
+    }
+
+    private void Onlogin(){
+        final String password = editTextPassword.getText().toString().trim();
+        final String email = editTextEmail.getText().toString().trim();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, REGISTER_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+//                        Toast.makeText(MainActivity.this,response,Toast.LENGTH_LONG).show();
+                        Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                        MainActivity.this.startActivity(homeIntent);
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this,error.toString(),Toast.LENGTH_LONG).show();
+                    }
+                }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("password",password);
+                params.put("username", email);
+                return params;
+            }
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == buttonRegister){
+            Onlogin();
+        }
+    }
+
+}
