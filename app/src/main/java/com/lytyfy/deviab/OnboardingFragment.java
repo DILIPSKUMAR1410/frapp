cb7e4ed30562080823ae6c6def65bd05867aae7a
DILIP S <dilipskumar1410@gmail.com>
2016-10-11 19:57:05 +0530

    list fragment

--- app/src/main/java/com/lytyfy/deviab/OnboardingFragment.java
+++ app/src/main/java/com/lytyfy/deviab/OnboardingFragment.java
@@ -1,18 +1,26 @@
 package com.lytyfy.deviab;
 
+import android.app.ListFragment;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
-public class OnboardingFragment extends Fragment {
+public class OnboardingFragment extends ListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-
         return inflater.inflate(
                 R.layout.fragment_onboarding, container, false);
+    }
+
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.Planets, R.layout.row_layout);
+        setListAdapter(adapter);
     }
 }
