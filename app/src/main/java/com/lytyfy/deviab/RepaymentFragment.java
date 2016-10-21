dd9fd7cfa7d6e23bb577173d287d58f0037db5a0
vasu dev <vasudv0912@gmail.com>
2016-10-21 20:52:42 +0530

    listview adaptor

--- app/src/main/java/com/lytyfy/deviab/RepaymentFragment.java
+++ app/src/main/java/com/lytyfy/deviab/RepaymentFragment.java
@@ -1,18 +1,40 @@
 package com.lytyfy.deviab;
 
+import android.app.ListFragment;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
-public class RepaymentFragment extends Fragment {
+import java.util.ArrayList;
+
+public class RepaymentFragment extends ListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
+    View v = inflater.inflate(
+            R.layout.fragment_repayment, container, false);
 
-        return inflater.inflate(
-                R.layout.fragment_repayment, container, false);
+        // Construct the data source
+        ArrayList<User> arrayOfUsers = new ArrayList<User>();
+// Create the adapter to convert the array to views
+        UsersAdapter adapter = new UsersAdapter(getActivity(), arrayOfUsers);
+// Attach the adapter to a ListView
+        ListView listView = (ListView) v.findViewById(android.R.id.list);
+        listView.setAdapter(adapter);
+
+        // Add item to adapter
+
+
+        for(int l=0; l<=5; l++){
+            User newUser = new User("Nathan", "San Diego");
+            adapter.add(newUser);
+        }
+
+
+
+        return v;
     }
 }
---
+++ app/src/main/java/com/lytyfy/deviab/User.java
@@ -0,0 +1,15 @@
+package com.lytyfy.deviab;
+
+/**
+ * Created by vasu on 21/10/16.
+ */
+
+public class User {
+    public String name;
+    public String hometown;
+
+    public User(String name, String hometown) {
+        this.name = name;
+        this.hometown = hometown;
+    }
+}
